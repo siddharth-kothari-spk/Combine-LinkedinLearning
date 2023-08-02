b9dd8030215e8b7a8897ed063b6e6be0c70081fe
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-02 17:36:18 +0530

    sample Future

--- Future & Just publisher.playground/Contents.swift
+++ Future & Just publisher.playground/Contents.swift
@@ -1,5 +1,6 @@
 import Combine
 import Foundation
+import UIKit
 
 // Just : A publisher that emits an output to each subscriber just once, and then finishes.
 let _ = Just("sample data").sink { strValue in
@@ -12,3 +13,28 @@ let subscriber = subject.sink { num in
     print(num)
 }
 Just(100).subscribe(subject)
+
+
+// Future : A publisher that eventually produces a single value and then finishes or fails.
+
+enum FutureError: Error {
+    case notMultiple
+}
+
+let future = Future<String, FutureError> { promise in
+    let calendar = Calendar.current
+    let second = calendar.component(.second, from: Date())
+    print("second is \(second)")
+    if second.isMultiple(of: 3){
+        promise(.success("We are successful: \(second)"))
+    }else{
+        promise(.failure(.notMultiple))
+    }
+}.catch{error in
+    Just("Caught the error")
+}
+.delay(for: .init(1), scheduler: RunLoop.main)
+.eraseToAnyPublisher()
+
+future.sink(receiveCompletion: {print($0)},
+            receiveValue: {print($0)})
