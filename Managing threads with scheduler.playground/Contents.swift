c7bf251d3505c5a9eebd42ed15de4283fdeafdd6
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-03 15:29:15 +0530

    Managing threads with schedulers

---
+++ Managing threads with scheduler.playground/Contents.swift
@@ -0,0 +1,32 @@
+import Foundation
+import Combine
+
+struct Post: Codable{
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
+print("Publisher: On main thread?: \(Thread.current.isMainThread)")
+print("Publisher: thread info: \(Thread.current)")
+
+let url = URL(string: "https://jsonplaceholder.typicode.com/posts")
+let queue = DispatchQueue(label: "a queue")
+
+let publisher = URLSession.shared.dataTaskPublisher(for: url!)
+    .map {$0.data}
+    .decode(type: Array<Post>.self, decoder: JSONDecoder())
+    //.subscribe(on: queue)
+
+let cancellableSink = publisher
+    .receive(on: DispatchQueue.main)
+    //.receive(on: DispatchQueue.global)
+    .sink(receiveCompletion: {completion in
+        print("Subscriber: On main thread?: \(Thread.current.isMainThread)")
+        print("Subscriber: thread info: \(Thread.current)")
+    }, receiveValue: {value in
+        print("Subscriber: On main thread?: \(Thread.current.isMainThread)")
+        print("Subscriber: thread info: \(Thread.current)")
+    })
+
