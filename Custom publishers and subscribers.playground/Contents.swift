6845d9995dc1f1ae3a632dd8130b5e1fe473bd2d
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-03 15:43:07 +0530

    changing methods to publisher and subscriber one

---
+++ Custom publishers and subscribers.playground/Contents.swift
@@ -0,0 +1,35 @@
+import Foundation
+import Combine
+
+extension Publisher {
+    
+    func isPrimeInteger<T: BinaryInteger>() -> Publishers.CompactMap<Self, T> where Output == T {
+        compactMap{ self.isPrime($0) }
+    }
+    func isPrime<T: BinaryInteger>(_ n: T) -> T? {
+        guard n != 2 else { return n }
+        guard n % 2 != 0 && n > 1 else { return nil }
+
+        var i = 3
+        while i * i <= n {
+            if (Int(n) % i) == 0 {
+                return nil
+            }
+            i += 2
+        }
+
+        return n
+    }
+}
+
+
+let numbers:[Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+//numbers.forEach{ value in
+//    if let primeValue = isPrime(value){
+//        print(primeValue)
+//    }
+//}
+
+numbers.publisher.isPrimeInteger().sink{print($0)}
+
